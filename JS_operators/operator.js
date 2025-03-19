99e97bc8d19b941192e508d5569ee76b6373ef05
ruchiraaa04 <ruchiramore04@gmail.com>
2025-03-19 23:48:58 +0530

    Updated files

---
+++ JS_operators/operator.js
@@ -0,0 +1,38 @@
+//Arithmetic Operators
+// let v1=20;
+// let v2=10;
+// console.log(v1+v2) //performs addition
+// console.log(v1-v2) //performs subtraction
+// console.log(v1*v2) //performs multiplication
+// console.log(v1/v2) //performs division i.e returns the quotient
+// console.log(v1%v2) //performs modulus i.e returns the remainder
+
+// console.log(10**2) //10 raise to 2, returns the power of that number
+
+//ASSIGNMENT OPERATORS
+let n = 10; //= is used to assign vale to variable
+n+=5; //5 is added to value stored in n i.e n=15
+n-=5 //15-5=10
+console.log(n)
+
+//COMPARISON OPERATORS
+let a=10;
+let b=20;
+console.log(a==b) //returns False, checks if two are equal
+console.log(a>=b) //returns False, checks if a is greater or equal
+console.log(a<=b) //returns True, checcks if a is less than or equal
+console.log(a!=b) //returns True, if a and b are not equal
+
+//LOGICAL OPERATORS
+let u = 50;
+console.log(u<60 && u>40) //returns true as 50 lies between 60 and 40
+
+//TERNARY OPERATORS
+let num1=10;
+let output=(num1 %2==0) ? "Even Number":"Odd Number";
+console.log(output)
+
+//STRING OPERATORS
+let s1="hello";
+let s2="js";
+console.log(s1+s2)
